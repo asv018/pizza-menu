1fe473fd998427d27bd87049696647e00ab6297f
Arpit Singh Verma <21bcs089@nith.ac.in>
2023-06-16 13:19:20 +0530

    Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,143 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const pizzaData = [
+    {
+      name: "Focaccia",
+      ingredients: "Bread with italian olive oil and rosemary",
+      price: 6,
+      photoName: "pizzas/focaccia.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Margherita",
+      ingredients: "Tomato and mozarella",
+      price: 10,
+      photoName: "pizzas/margherita.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Spinaci",
+      ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+      price: 12,
+      photoName: "pizzas/spinaci.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Funghi",
+      ingredients: "Tomato, mozarella, mushrooms, and onion",
+      price: 12,
+      photoName: "pizzas/funghi.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Salamino",
+      ingredients: "Tomato, mozarella, and pepperoni",
+      price: 15,
+      photoName: "pizzas/salamino.jpg",
+      soldOut: true,
+    },
+    {
+      name: "Pizza Prosciutto",
+      ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+      price: 18,
+      photoName: "pizzas/prosciutto.jpg",
+      soldOut: false,
+    },
+  ];
+  
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App(){
+    return (
+    <div className="container">
+    <Header/>
+    <Menu/>
+    <Footer/>
+    </div>
+    );
+}
+
+
+function Header () {
+  // const style = {color: 'red' , fontSize : "48px" , textTransform : "uppercase"};
+  const style = {};
+    return(
+      <header  className= "header footer">
+     <h1 style={style}>Fast React Pizza Co.</h1>
+     </header>
+    );
+}
+
+function Menu () {
+  const pizzas = pizzaData;
+    return(
+    <main className='menu'>
+
+     <h2>Our menu</h2>
+    
+    {pizzas &&  (  <ul className="pizzas">
+      {pizzas.map(pizza => <Pizza pizzaObj = {pizza} key = {pizza.name}/>)}
+
+     </ul> )}
+ 
+     {/* <Pizza name = 'Pizza Spinaci' ingredients = 'omato, mozarella, spinach, and ricotta cheese'
+     photoName = 'pizzas/spinaci.jpg'
+     price = {10} />
+
+     <Pizza name = "Pizza Funghi" ingredients = 
+     "Tomato , mushrooms"
+      price = {12}
+     photoName = "pizzas/funghi.jpg" /> */}
+
+     </main>
+    );
+}
+
+function Pizza(props){
+ console.log(props);
+  return (
+  <li className="pizza">
+      <img src = {props.pizzaObj.photoName} alt = {props.pizzaObj.name}/>
+      <div>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price}</span>
+      </div>
+  {/* <h3>{props.pizzaObj.name}</h3>
+  <p>{props.pizzaObj.ingredients}</p> */}
+  </li>
+  );
+}
+
+// props - btw parent and child component how we pass data
+
+function Footer () {
+    const hour = new Date().getHours()
+    const openHour = 12;
+    const closeHour = 22;
+    const isOpen = hour >= openHour && hour <= closeHour;
+    console.log(isOpen);
+
+    // if(hour >= openHour && hour <= closeHour) alert("We're currently open!");
+    // else alert("Sorry we're closed");
+
+    return <footer className="footer">
+      {isOpen && (
+      <div className="order">
+      <p>We're open until {closeHour}:00. Come visit us or order online.
+      </p>
+      <button className="btn">Order</button>
+      </div>
+      )}
+      </footer>
+    // return React.createElement('footer' , null , "We're currently open!")
+}
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<React.StrictMode>
+    <App/>
+</React.StrictMode>);
